d7445138b4aee403c2d412ccc9b690df01c193f8
zhaolinmao <zhaolinmao2014>
2016-12-27 10:56:29 +0800

    这才是 真正的第一版

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* POST users listing. */
+router.post('/', function(req, res, next) {
   res.render('fileUpload');
+});
+
+/* POST users listing. */
+router.post('/add', function(req, res, next) {
+  var form  = new formidable.IncomingForm();
+       form.encoding = 'utf-8';		//设置编辑
+       form.uploadDir = 'fileupload';	 //设置上传目录
+       form.keepExtensions = true;	 //保留后缀
+       form.maxFieldsSize = 20 * 1024 * 1024;   //文件大小
+
+      form.parse(req, function(err, fields, files) {
+        fs.renameSync(files.file.path,files.file.name);
+      });
+      res.json(200);
 });
 
 module.exports = router;
